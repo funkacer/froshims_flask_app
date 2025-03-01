b38ed05d2020217bad54fe1f5f9f098aabd0779e
Jiří Černý <funkacer@gmail.com>
2025-03-01 17:47:02 +0100

    Registration form post

--- app.py
+++ app.py
@@ -8,3 +8,11 @@
 # touch layout.html index.html
 # flask run
 
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # return "hello"
+    return render_template("index.html")
