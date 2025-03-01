d32ed0bcc382b61c3b8a297f2a2f5f5900cbc2f1
Jiří Černý <funkacer@gmail.com>
2025-03-01 17:32:00 +0100

    Init commit

---
+++ app.py
@@ -0,0 +1,10 @@
+# mkdir froshims_flask_app
+# cd froshims_flask_app
+# cp ../my_flask_app/requirements.txt .
+# . ../../bin/activate (. _Git/_test/bin/activate)
+# pip install -r requirements.txt
+# touch app.py
+# mkdir templates
+# touch layout.html index.html
+# flask run
+
