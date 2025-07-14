bfe3ac4ec0946ad732110e395c54eb4cf067471a
Jiří Černý <funkacer@gmail.com>
2025-07-14 11:53:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,6 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
-    if not request.form.get("name") or not request.form.get("sport"):
+    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
         return render_template("failure.html")
     return render_template("success.html")
