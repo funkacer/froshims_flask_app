e87e49376c24b7dead6a32bb54d4844467fa7f32
Jiří Černý <funkacer@gmail.com>
2025-07-15 17:16:25 +0200

    Almost final version (do styling)

--- app.py
+++ app.py
@@ -9,8 +9,13 @@
 # flask run
 
 from flask import Flask, render_template, request
+import sqlite3
+from collections import namedtuple
+import os
 
 app = Flask(__name__)
+
+DB = "froshims.db"
 
 SPORTS = [
     "Basketball",
@@ -18,9 +23,21 @@ SPORTS = [
     "Ultimate Frisbee",
 ]
 
+SEX = {1: 'Male', 0: 'Female', None: 'N/A'}
+
+REGISTRANTS = [
+    {'id': 1, 'name': 'já', 'sport': 'fotbal'},
+    {'id': 2, 'name': 'ty', 'sport': 'hokej'},
+]
+
+def namedtuple_factory(cursor, row):
+    fields = [column[0] for column in cursor.description]
+    cls = namedtuple("Row", fields)
+    return cls._make(row)
+
 @app.route("/")
 def index():
-    return render_template("index.html", sports=SPORTS)
+    return render_template("index.html", sports=SPORTS, pwd=os.getcwd())
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -32,4 +49,18 @@ def register():
         return render_template("error.html", message="Missing sport")
     if sport not in SPORTS:
         return render_template("error.html", message="Invalid sport")
+    sex = next(filter(lambda x: SEX[x]==request.form.get("sex"), SEX.keys()), None)
+    con = sqlite3.connect(DB)
+    with con:
+        con.execute("INSERT INTO registrants (name, sport, sex) VALUES (?, ?, ?)", [name, sport, sex])
+    con.close()
     return render_template("success.html")
+
+@app.route("/registrants")
+def registrants():
+    con = sqlite3.connect(DB)
+    con.row_factory = sqlite3.Row
+    cur = con.execute("SELECT * from registrants")
+    registrants = cur.fetchall()
+    con.close()
+    return render_template("registrants.html", registrants=registrants, SEX=SEX)
---
+++ froshims.py
@@ -0,0 +1,4 @@
+import sqlite3
+con = sqlite3.connect("froshims.db")
+con.execute("CREATE TABLE registrants (id INTEGER, name TEXT NOT NULL, sport TEXT NOT NULL, sex BOOL, PRIMARY KEY (id));")
+con.close()
